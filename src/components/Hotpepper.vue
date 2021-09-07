cd736a99a4c49edf58fec96c58bb04c859d54f80
serinazunazuna <seririn1999@gmail.com>
2021-09-07 23:59:40 +0900

    ホットペッパーAPIの情報絞りました！

--- src/components/Hotpepper.vue
+++ src/components/Hotpepper.vue
@@ -1,34 +1,35 @@
 <template>
   <div>
-    <html lang="ja">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Fetch JSON</title>
-      </head>
-      <body>
-        <h1>{{ date }}</h1>
-        <div>
-          <img src="" alt="お店の名前" id="food-image" />
-        </div>
-      </body>
-    </html>
+    <div v-for="food in all" :key="food.id">
+      {{ food.name }}
+    </div>
+    <div>
+      <img src="" alt="お店の名前" id="food-image" />
+    </div>
   </div>
 </template>
 
 <script>
-const imageElement = document.getElementById("food-image");
-
-fetch(
-  "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=703a952c5ae9eaa7&large_area=Z011"
-)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    imageElement.src = data.message;
-  });
+export default {
+  data() {
+    return { all: [] };
+  },
+  methods: {
+    getrestaurant: function() {
+      this.axios
+        .get(
+          "hotpepper/gourmet/v1/?key=703a952c5ae9eaa7&large_area=Z011&format=json"
+        )
+        .then((res) => res.data)
+        .then((value) => {
+          this.all = value.results.shop;
+        });
+    },
+  },
+  mounted() {
+    this.getrestaurant();
+  },
+};
 </script>
 
 <style></style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    proxy: "https://webservice.recruit.co.jp",
+  },
+};
