54207daf0b4aa9c7d5526e23eec49786462b07bb
serinazunazuna <seririn1999@gmail.com>
2021-09-07 10:38:05 +0900

    ホットペッパーAPIエラー出てます、、

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hotpepper msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
+import Hotpepper from "./components/Hotpepper.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Hotpepper,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Hotpepper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <html lang="ja">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Fetch JSON</title>
+      </head>
+      <body>
+        <h1>{{ date }}</h1>
+        <div>
+          <img src="" alt="お店の名前" id="food-image" />
+        </div>
+      </body>
+    </html>
+  </div>
+</template>
+
+<script>
+const imageElement = document.getElementById("food-image");
+
+fetch(
+  "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=703a952c5ae9eaa7&large_area=Z011"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    imageElement.src = data.message;
+  });
+</script>
+
+<style></style>
