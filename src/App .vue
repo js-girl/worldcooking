97783238081b70fca89908c6f352051671c4835b
mskk0803 <toyama2ura@gmail.com>
2021-09-08 10:04:34 +0900

    mergeできない

---
+++ src/App .vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <GoogleMap />
+    <Wasyoku />
+  </div>
+</template>
+
+<script>
+import Wasyoku from "./components/EastAsia/Wasyoku.vue"
+import GoogleMap from "./components/Map.vue"
+export default {
+  name: "App",
+  components: {
+    Wasyoku,
+    GoogleMap,
+  },
+}
+</script>
+
+<style></style>
